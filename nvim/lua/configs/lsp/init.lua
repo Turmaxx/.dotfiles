627fe1c9ae30814b3b238973fca02de6bd557479
turmaxx <88465473+turmaxx@users.noreply.github.com>
2022-12-22 19:04:00 -0500

    Old LSP removed

--- nvim/lua/configs/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
--- require "brook.lsp.configs"
-require("brook.lsp.handlers").setup()
-require("brook.lsp.lsp-installer")
--- require "brook.lsp.null-ls"
