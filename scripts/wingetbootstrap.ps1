76d68355eaa3585f43f92417c6a81b87393a6065
Turmaxx <88465473+Turmaxx@users.noreply.github.com>
2023-06-25 07:08:43 -0400

    updated the bootstrap and changed file to powershell

---
+++ scripts/wingetbootstrap.ps1
@@ -0,0 +1,52 @@
+# This is a script to duplicate all the apps that I use on a daily basis on my windows machine to any new windows machine that I am on
+# Written on June 06, 2023
+# Revised and Updated Version on June 25, 2023
+
+# Windows Apps: Note to future-self below are app-ids that winget uses
+$apps = @(
+    # Web Browers
+    "Mozilla.Firefox"
+    "Google.Chrome"
+
+    # Text Editors and Development
+    "Neovim.Neovim"
+    "Microsoft.VisualStudioCode"
+    "Python.Python.3.11"
+    "Docker.DockerDesktop"
+    "Git.Git"
+    "JesseDuffield.lazygit"
+
+    # Note-Taking
+    "Notion.Notion"
+    "Obsidian.Obsidian"
+    
+    # Messaging and Meetings
+    "Discord.Discord"
+    "WhatsApp.WhatsApp"
+    "Telegram.TelegramDesktop"
+    "Microsoft.Teams"
+    "SlackTechnologies.Slack"
+    "Zoom.Zoom"
+
+    # Entertainment
+    "Valve.Steam"
+    "Spotify.Spotify"
+
+    # VPN
+    "ProtonTechnologies.ProtonVPN"
+
+    # Productvity
+    "Doist.Todoist"
+    "Foxit.PhantomPDF"
+
+    # Hardware
+    "Logitech.OptionsPlus"
+)
+
+# FLAGS
+$ID = "-e", "--id"
+$PROMPT = "--accept-package-agreements", "--accept-source-agreements"
+
+foreach ($app in $apps) {
+    winget install $ID $app $PROMPT
+}
