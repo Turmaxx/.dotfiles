e7ba6cae60f71ce4d18b0549aaf0be3626911182
Turmaxx <88465473+Turmaxx@users.noreply.github.com>
2023-07-20 19:38:52 +0300

    Formatted init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,30 @@
 -- INIT.LUA
 function run(core_modules)
-	for _, module in ipairs(core_modules) do
-		local ok, err = pcall(require, module)
-		if not ok then
-			return
-		end
-	end
+    for _, module in ipairs(core_modules) do
+        local ok, err = pcall(require, module)
+        if not ok then
+            return
+        end
+    end
 end
 
 local core_modules = {
-	"brook/options",
-	"brook/keymaps",
-	"brook/plugins",
+    "brook/options",
+    "brook/keymaps",
+    "brook/plugins",
 
     "configs/impatient",
     "configs/completion",
     "configs/comments",
     "configs/gitsigns",
-	"configs/telescope",
+    "configs/telescope",
     "configs/statusline",
     "configs/alpha",
-	"configs/nvim-tree",
-	"configs/toggleterm",
-	"configs/colorizer",
-	"configs/autopairs",
-	"configs/colorscheme",
+    "configs/nvim-tree",
+    "configs/toggleterm",
+    "configs/colorizer",
+    "configs/autopairs",
+    "configs/colorscheme",
     "configs/lsp",
     "configs/treesitter"
 }
