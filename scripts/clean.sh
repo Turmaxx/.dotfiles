13ebc237b2b945e569b47d84a128475a4236c414
Turmaxx <88465473+Turmaxx@users.noreply.github.com>
2023-08-06 13:20:17 +0300

    chmodded clean.sh, permitDocker.sh and wingetbootstrap.ps1

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# If you're having a problem or some kinda of an error with a plugin, run this script
+# If you're having a problem or some kinda of an error with a plugin on neovim, run this script
 rm -rf ~/.cache/nvim
 rm -rf ~/.config/nvim/plugin
 rm -rf ~/.local/share/nvim
-nvim 
+nvim
+echo neovim has been cleaned
