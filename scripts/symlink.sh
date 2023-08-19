115b28441cd704b0430eda3a689c61bbd7f53a1a
Turmaxx <88465473+Turmaxx@users.noreply.github.com>
2023-08-19 13:37:44 +0300

    symlink script to get things set up

---
+++ scripts/symlink.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env
+
+ln -s ~/Projects/dots/nvim/ ~/.config/nvim/
+ln -s ~/Projects/dots/.config/alacritty/ ~/.config/alacritty/
+ln -s ~/Projects/dots/.config/wezterm/ ~/.config/wezterm/
+ln -s ~/Projects/dots/.config/helix/ ~/.config/helix/
+ln -s ~/Projects/dots/.config/starship/ ~/.config/starship/
+ln -s ~/Projects/dots/.config/zsh/ ~/.config/zsh/
+ln -s ~/Projects/dots/.config/bash/ ~/.config/bash/
+ln -s ~/Projects/dots/.config/tmux/ ~/.config/tmux/
